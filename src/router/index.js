08f76ab231b5d7ac9aff063928c2a124120102ad
LorenzovandenDungen <2130766@talnet.nl>
2024-03-19 10:49:40 +0100

    save

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PackageSelectionView from '../views/PackageSelectionView.vue' // Importeer uw nieuwe view
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/packages',
+    name: 'packages',
+    component: PackageSelectionView // Voeg een nieuwe route toe
+  }
+  // Voeg meer routes toe indien nodig
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  history: createWebHistory(process.env.BASE_URL),
+  routes
 })
 
 export default router
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import PackageSelection from '../components/PackageSelection.vue' // Importeer PackageSelection
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <PackageSelection />
+    <!-- Voeg PackageSelection toe aan uw template -->
   </main>
 </template>
