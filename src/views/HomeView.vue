f33fe31986138fcb5ff2db95a5d0bb8bd6ba962a
LorenzovandenDungen <2130766@talnet.nl>
2024-03-20 11:37:00 +0100

    SAVE

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 import PackageSelection from '../components/PackageSelection.vue' // Importeer PackageSelection
 </script>
 
 <template>
   <main>
-    <TheWelcome />
     <PackageSelection />
     <!-- Voeg PackageSelection toe aan uw template -->
   </main>
