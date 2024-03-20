71aa335642878ee3ea3d8093c0a2125d5dbc6815
LorenzovandenDungen <2130766@talnet.nl>
2024-03-20 13:43:26 +0100

    save

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import HomeView from '../views/HomeView.vue'
 import TarievenView from '../views/TarievenView.vue'
 import OfferteView from '../views/OfferteView.vue'
 import ContactView from '../views/ContactView.vue'
+import WebsitesView from '../views/WebsitesView.vue' // Added line
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
   { path: '/tarieven', name: 'tarieven', component: TarievenView },
   { path: '/offerte', name: 'offerte', component: OfferteView },
-  { path: '/contact', name: 'contact', component: ContactView }
+  { path: '/contact', name: 'contact', component: ContactView },
+  { path: '/websites', name: 'websites', component: WebsitesView } // Added line
   // Add other routes here if necessary
 ]
 
